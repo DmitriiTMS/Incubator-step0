450f25f77a3e1ce29e4e5e88ec183fd8d9a39e30
DmitriiTMS <dmitrii12_88@mail.ru>
2025-02-28 20:11:31 +0300

    update settings

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import {app} from './app'
 import { SETTINGS } from './settings/settings'
 
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-    console.log(`...server started in port ${port}`)
+app.listen(SETTINGS.PORT, () => {
+    console.log(`...server started in port ${SETTINGS.PORT}`)
 })
